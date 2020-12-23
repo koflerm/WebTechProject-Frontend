9ffd338e7b967e1c5f290f066f787778db4b1590
Markus Kofler <koflerm@outlook.com>
2020-12-23 14:18:21 +0100

    Add Images to Overview Page

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent {
+  images = [
+    { src: 'assets/img/handshake.jpg', title: 'Trusted by 1.000.000+ customers' },
+    { src: 'https://wallpapercrafter.com/desktop/292420-dog-friendship-nature-trust-labrador-snout.jpg', title: 'We like dogs.' },
+    { src: 'https://pixelz.cc/wp-content/uploads/2018/09/digital-security-lock-uhd-4k-wallpaper.jpg', title: 'Your data is highly secured.' },
+    { src: 'https://www.itl.cat/pngfile/big/303-3032161_donald-trump-wallpaper-background-kim-jong-un-ok.jpg', title: 'Recommended by celebrities.' }
+  ];
 
-  constructor() { }
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
 
-  ngOnInit(): void {
+  @ViewChild('carousel', { static: true })
+  carousel!: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
   }
 
 }
--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { OverviewRoutingModule } from './overview-routing.module';
 import { OverviewComponent } from './overview.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [OverviewComponent],
   imports: [
     CommonModule,
-    OverviewRoutingModule
+    OverviewRoutingModule,
+    NgbModule
   ]
 })
 export class OverviewModule { }
